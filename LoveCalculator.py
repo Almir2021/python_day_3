dbef63c0266fc543988c61fc19483c2ac6f2429c
Almir2021 <bosnjicalmir@gmail.com>
2023-08-12 23:15:10 +0200

    just started no idea no metod how to makeit works

---
+++ LoveCalculator.py
@@ -0,0 +1,11 @@
+# 🚨 Don't change the code below 👇
+print("Welcome to the Love Calculator!")
+name1 = input("What is your name? \n")
+name2 = input("What is their name? \n")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+
+Love = {"l""o" "v" "e"}
+print(love)
