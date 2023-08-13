7e73956c7e80641aeb8ced9676f702971ede0c45
Almir2021 <bosnjicalmir@gmail.com>
2023-08-13 21:16:47 +0200

    am getting there

--- LoveCalculator.py
+++ LoveCalculator.py
@@ -4,13 +4,42 @@ name1 = input("What is your name? \n")
 name2 = input("What is their name? \n")
 # 🚨 Don't change the code above 👆
 
-#Write your code below this line 👇
+#Write your code below this line 
+programming_language_list = name1
+name1 = name1.lower()
+
+name2 = name2.lower()
+
+
+
 True1 = ["t","r" ,"u" ,"e"]
-for x in True1:
- print(x)
+for count1 in True1:
+ print(programming_language_list.count(count1)) 
+ print(count1)
 
 
 
 Love = ["l","o" ,"v" ,"e"]
-for x in Love:
- print(x)
+for count2 in Love:
+ print(count2)
+
+
+
+programming_language = name1
+
+programming_language_list = list(programming_language)
+
+print(programming_language_list)
+
+
+
+
+programming_language2 = name2
+
+programming_language2_list = list(programming_language2)
+
+print(programming_language2_list)
+ 
+print(programming_language_list.count(count)) 
+
+
